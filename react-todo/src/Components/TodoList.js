664c3fec90bbe261f38ad6ce9dfcbe3965aefce9
ouldjelloulsara@gmail.com <ouldjelloulsara@gmail.com>
2020-12-11 18:01:29 +0100

    remove list of todo

--- react-todo/src/Components/TodoList.js
+++ react-todo/src/Components/TodoList.js
@@ -15,6 +15,13 @@ function TodoList() {
     console.log(...todos);
     } 
 
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr);
+
+       };
+
     const completeTodo = id =>{
         let updateTodos = todos.map(todo => {
             if (todo.isComplete = !todo.isComplete)
@@ -29,6 +36,7 @@ function TodoList() {
              <Todo 
                 todos={todos}
                 completeTodo={completeTodo}
+                removeTodo={removeTodo}
              />
         </div>
     )
